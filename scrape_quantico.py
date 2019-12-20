c959c12c7dff05e08ece81f5bb72b0ac5d82d36a
Mark Shoun <mark.shoun@gmail.com>
2019-12-19 22:41:18 -0500

    Add incremental update to QOC scrape
    Added the ability to specify individual row numbers in the QOC input
    file to scrape. If such numbers are specified, only those rows are
    pulled, then the results are appended to existing results.
    
    This is a big time-saver for adding the most recent event.

--- scrape_quantico.py
+++ scrape_quantico.py
@@ -4,19 +4,31 @@ The input file data/quantico_events.csv controls the list of events for which re
 The output is five separate CSV files listed at the bottom of the script.
 """
 
-
 import csv
 import datetime
+import sys
 
 from scrape import quantico
 
 
-def dump_csv(filename, rows):
+def append_csv(filename, rows):
+    with open(filename, "r", newline="") as infile:
+        reader = csv.reader(infile)
+        old_rows = [row for row in reader]
+    with open(filename, "w", newline="") as outfile:
+        writer = csv.writer(outfile)
+        for row in old_rows + rows:
+            writer.writerow(row)
+
+
+def write_csv(filename, rows):
     with open(filename, "w", newline="") as outfile:
         writer = csv.writer(outfile)
         for row in rows:
             writer.writerow(row)
 
+
+row_ids = [int(arg) for arg in sys.argv[1:]]
 
 with open("data/quantico_events.csv", newline="") as infile:
     reader = csv.reader(infile)
@@ -24,6 +36,10 @@ with open("data/quantico_events.csv", newline="") as infile:
     rows = [(datetime.datetime.strptime(row[0], "%Y-%m-%d").date(), row[1], row[2],
              row[3], None if row[4] == "" else row[4], None if row[5] == "" else row[5])
             for row in reader]
+
+# Filter to specific rows if specified
+if row_ids:
+    rows = [row for id, row in enumerate(rows) if id in row_ids]
 
 event_rows, course_rows, start_rows, leg_rows, split_rows = [], [], [], [], []
 for row in rows:
@@ -39,8 +55,13 @@ for row in rows:
     leg_rows += le
     split_rows += sp
 
-dump_csv("data/events.csv", event_rows)
-dump_csv("data/courses.csv", course_rows)
-dump_csv("data/starts.csv", start_rows)
-dump_csv("data/legs.csv", leg_rows)
-dump_csv("data/splits.csv", split_rows)
+if row_ids:
+    save_fn = append_csv
+else:
+    save_fn = write_csv
+
+save_fn("data/events.csv", event_rows)
+save_fn("data/courses.csv", course_rows)
+save_fn("data/starts.csv", start_rows)
+save_fn("data/legs.csv", leg_rows)
+save_fn("data/splits.csv", split_rows)
